29c67cba6bfa9e52afd62aa71595125f44b53e60
Noah Orr <Tallons27@yahoo.com>
2021-01-27 01:13:27 -0700

    FoodService

---
+++ BlazorBattles/Client/Services/FoodService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+	public class FoodService : IFoodService
+	{
+		public event Action OnChange;
+		public int Food { get; set; } = 1000;
+
+		public void EatFood(int amount)
+		{
+			Food -= amount;
+			FoodChanged();
+		}
+		public void AddFood(int amount)
+		{
+			Food += amount;
+			FoodChanged();
+		}
+
+		void FoodChanged() => OnChange.Invoke();
+	}
+}
---
+++ BlazorBattles/Client/Services/IFoodService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+	public interface IFoodService
+	{
+		event Action OnChange;
+		int Food { get; set; }
+		void EatFood(int amount);
+		void AddFood(int amount);
+	}
+}
