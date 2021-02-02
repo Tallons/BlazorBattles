26c8795933e7e9f78e9e5fa71b3d043e0f33c3dd
Tallons <Tallons27@yahoo.com>
2021-02-02 12:30:56 -0700

    Basic Authentication

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -8,7 +8,9 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
 using Blazored.Toast;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BlazorBattles.Client
 {
@@ -20,9 +22,13 @@ namespace BlazorBattles.Client
 			builder.RootComponents.Add<App>("#app");
 
 			builder.Services.AddBlazoredToast();
+			builder.Services.AddBlazoredLocalStorage();
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 			builder.Services.AddScoped<IFoodService, FoodService>();
 			builder.Services.AddScoped<IUnitService, UnitService>();
+			builder.Services.AddOptions();
+			builder.Services.AddAuthorizationCore();
+			builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 			await builder.Build().RunAsync();
 		}
---
+++ BlazorBattles/Shared/UserRegister.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBattles.Shared
+{
+    public class UserRegister
+    {
+        [Required, EmailAddress] 
+        public string Email { get; set; }
+        
+        [StringLength(16, ErrorMessage = "Your username is too long (16 characters max).")]
+        public string Username { get; set; }
+        
+        public string Bio { get; set; }
+        
+        [Required, StringLength(100, MinimumLength = 8)]
+        public string Password { get; set; }
+        
+        [Compare("Password", ErrorMessage = "The passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+        
+        public int Food { get; set; } = 500;
+        
+        public string StartUnitId { get; set; } = "1";
+        
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
+        
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Only confirmed users can play!")]
+        public bool IsConfirmed { get; set; } = true;
+    }
+}
